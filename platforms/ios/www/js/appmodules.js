b338d57af2775e707c8f2f38f65c8ce3447d5356
futuravis <futuravis@github.com>
2018-02-02 08:29:59 +0530

    Integrating material css plugin
    Integrating material css plugin

---
+++ platforms/ios/www/js/appmodules.js
@@ -0,0 +1,22 @@
+function displayLoginPage()
+{
+
+    
+    
+    
+    var html = "";
+    html += '<form>';
+    html += '<div><div class="row"><div class="col s1">1</div><div class="col s1">2</div><div class="col s1">3</div><div class="col s1">4</div><div class="col s1">5</div><div class="col s1">6</div><div class="col s1">7</div><div class="col s1">8</div><div class="col s1">9</div><div class="col s1">10</div><div class="col s1">11</div><div class="col s1">12</div></div>';
+    html += '</div>';
+    html +='</form>';
+
+
+    window.setTimeout(function() {
+                      $('#login').empty();
+                      $('#login').append(html).trigger('create');
+                      $('#login').css("display","block");
+                      window.location.href = '#login';
+                      }, 200);
+
+    
+}
