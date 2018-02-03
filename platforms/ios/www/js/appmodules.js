d5b3082a8c7e2e1b4489752871de27c73c53ea58
futuravis <futuravis@github.com>
2018-02-04 01:12:36 +0530

    Fixed the footer issues and header issues.
    > Alinged the footer
    > alinged the header.
    > Added new icons.

--- platforms/ios/www/js/appmodules.js
+++ platforms/ios/www/js/appmodules.js
@@ -2,92 +2,116 @@ function displayHomePage()
 {
     
     var symbolJSon = [
-                      {"symbol":"img/Profile.png"},
-                      {"symbol":"img/Settings.png"},
-                      {"symbol":"img/Highlyrated.png"},
-                      {"symbol":"img/MostPopular.png"},
-                      {"symbol":"img/newlogo.png"},
-                      {"symbol":"img/registergreyn.png"},
-                      {"symbol":"img/Analytics.png"},
-                      {"symbol":"img/Myclasses.png"},
-                      {"symbol":"img/My-Team.png"},
-                      {"symbol":"img/mic_white.png"},
-                      {"symbol":"img/my_dashboard.png"}
+                      {"symbol":"img/user.png"},
+                      {"symbol":"img/search.png"},
+                      {"symbol":"img/Categories.png"},
+                      {"symbol":"img/home.png"},
+                      {"symbol":"img/logo.png"},
+                      {"symbol":"img/add.png"},
+                      {"symbol":"img/filter.png"},
                       ];
-    
-    var json_Profile= {
-        "name": "My Profile",
+    var json_My_Account= {
+        "name": "My Account",
         "symbol": symbolJSon[0].symbol,
         "method": "",
-        "id":"footer_My_Profile_icon",
+        "id":"footer_My_Account",
     }
-    var json_Settings= {
-        "name": "Settings",
+    var json_Search= {
+        "name": "Search",
         "symbol": symbolJSon[1].symbol,
         "method": "",
-        "id":"footer_Settings_icon",
+        "id":"footer_Search",
     }
-    var json_Highly_Rated= {
-        "name": "Highly Rated",
+    var json_Categories= {
+        "name": "Categories",
         "symbol": symbolJSon[2].symbol,
         "method": "",
-        "id":"footer_Highly_Rated_icon",
+        "id":"footer_Categories",
     }
-    var json_Popular= {
-        "name": "Popular",
+    var json_Home= {
+        "name": "Home",
         "symbol": symbolJSon[3].symbol,
         "method": "",
-        "id":"footer_Popular_icon",
+        "id":"footer_Home",
     }
 
-    var Footter_home_icons = {
-        "jsonFormat": [
-                       json_Profile,
-                       json_Settings,
-                       json_Highly_Rated,
-                       json_Popular,
-                       ]
-    }
-    
     var json_Logo = {
         "name": "Logo",
         "symbol": symbolJSon[4].symbol,
         "method": "",
-        "id":"footer_Popular_icon",
+        "id":"footer_Logo",
     }
-    var json_Create = {
-        "name": "Create",
+    var json_Add = {
+        "name": "Add",
         "symbol": symbolJSon[5].symbol,
         "method": "",
-        "id":"footer_Popular_icon",
+        "id":"footer_Add",
+    }
+    
+    
+    var Footter_home_icons = {
+        "jsonFormat": [
+                       json_Home,
+                       json_Categories,
+                       json_Search,
+                       json_My_Account,
+                       ]
     }
 
     var header_home_icons = {
         "jsonFormat": [
                        json_Logo,
-                       json_Create,
+                       json_Add,
                        ]
     }
 
     
-    
-    var Html = '<form>';
+    var Html = '';
+    Html += '<form>';
     Html += displayHeader(header_home_icons);
-
     Html += '<div class="content">'
-
     Html += '</div>'
-
     Html += displayFooter(Footter_home_icons);
     Html +='</form>';
     
     
     window.setTimeout(function() {
-                      $('#login').empty();
-                      $('#login').append(Html).trigger('create');
-                      $('#login').css("display","block");
-                      window.location.href = '#login';
+                      $('#home').empty();
+                      $('#home').append(Html).trigger('create');
+                      $('#home').css("display","block");
+                      window.location.href = '#home';
                       }, 200);
+}
+
+function displayHeader(header_home_icons)
+{
+    var html = "";
+    html += '<div>'
+    html += '<div class="header home_header sticky_header" id="myHeader">'
+    html += '<div class="row fixed_header_row">'
+    
+    //Create the add
+    html += '<div class="col s3 center-align fixed_header_col">'
+    html += '<img class="header_log_img" src="'+header_home_icons.jsonFormat[0].symbol+'">'
+    html += '</div>'
+    
+    html += '<div class="col s6 fixed_header_col">'
+        html += '</div>'
+
+    html += '<div class="col s3 fixed_header_col">'
+    html += '<div class="row fixed_header_row">'
+    html += '<div class="col s8 fixed_header_col header_add_text center-align">'+header_home_icons.jsonFormat[1].name+'</div>'
+    html += '<div class="col s4 fixed_header_col position_relative">'
+    html += '<img class="header_add_img" src="'+header_home_icons.jsonFormat[1].symbol+'">'
+    html += '</div>'
+    html += '</div>'
+    html += '</div>'
+    
+    
+    html += '</div>'
+    html += '</div>'
+    html += '</div>'
+    return html;
 }
 
 
@@ -105,7 +129,7 @@ function displayFooter(Footter_home_icons)
     html += '<footer>'
     html += '<div class="row fixed_footer_row">'
     for(var j=0;j<Footter_home_icons.jsonFormat.length;j++){
-    html += '<div class="col s3 waves-effect waves-light" onclick="'+Footter_home_icons.jsonFormat[j].method+'">'
+    html += '<div class="col s3 fixed_footer_col waves-effect waves-light" onclick="'+Footter_home_icons.jsonFormat[j].method+'">'
     html += '<div class="row fixed_footer_icon_row">'
     html += '<img class="fixed_footer_image" src="'+Footter_home_icons.jsonFormat[j].symbol+'">'
     html += '</div>'
@@ -117,38 +141,6 @@ function displayFooter(Footter_home_icons)
     return html;
 }
 
-
-function displayHeader(header_home_icons)
-{
-    //alert(header_home_icons.jsonFormat[1].name)
-    var html = "";
-    html += '<div>'
-    html += '<div class="header home_header sticky_header" id="myHeader">'
-    html += '<div class="row">'
-    
-    //Create the add
-    html += '<div class="col s4">'
-    html += '<div class="row">'
-    
-    html += '<div class="col s8">'+header_home_icons.jsonFormat[1].name+'</div>'
-    html += '<div class="col s4">'
-    html += '<img class="fixed_footer_image" src="'+header_home_icons.jsonFormat[1].symbol+'">'
-    html += '</div>'
-    
-    html += '</div>'
-    html += '</div>'
-
-    
-    
-    html += '<div class="col s6">2</div>'
-    html += '<div class="col s2">3</div>'
-
-    
-    html += '</div>'
-    html += '</div>'
-    html += '</div>'
-    return html;
-}
 
 
 
