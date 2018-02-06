827b907aa491b4a7ae5f07f9d5d251e7e46b47bf
futuravis <futuravis@github.com>
2018-02-07 00:24:53 +0530

    Register page ui
    Completed register page ui
    Transition between login and register page done.

--- platforms/ios/www/js/login.js
+++ platforms/ios/www/js/login.js
@@ -1,6 +1,12 @@
 
-function displayLoginPage()
+function displayLoginPage(Prev_page)
 {
+    if(Prev_page){
+        $('#register').hide();
+        $('#login').show();
+        slide_page('front','login')
+    }
+
     var Html = '';
     
     //Register card
@@ -16,13 +22,11 @@ function displayLoginPage()
     Html += '</div>'
     Html += '</div>'
     
-    
     Html += '</div>'
     Html += '</div>'
     Html += '</div>'
     Html += '</div>'
     Html += '</div>'
-    
     
     //Login card
     Html += '<div class="row">'
@@ -72,14 +76,13 @@ function displayLoginPage()
     Html += '<div class="card-stacked">'
     Html += '<div class="card-action">'
     
-    
     Html += '<div class="row">'
     Html += '<div class=" col s12">'
     Html += '<label>Not a member yet ?</label>'
     Html += '</div>'
     Html += '</div>'
     
-    Html += '<div class="row ">'
+    Html += '<div class="row " onclick="displayregisterpage();">'
     Html += '<div class=" col s12 waves-effect waves-light login_button memeber_now_button_colour ">'
     Html += '<div class="row">Member Now</div>'
     Html += '</div>'
@@ -100,6 +103,121 @@ function displayLoginPage()
     
     
 }
+
+
+
+
+function displayregisterpage(){
+    $('#login').hide();
+    $('#register').show();
+    
+    slide_page('front','register')
+    
+    var Html = '';
+    
+    //Register card
+    Html += '<div class="row ">'
+    Html += '<div class="col s12">'
+    Html += '<div class="card horizontal z-depth-4">'
+    Html += '<div class="card-stacked">'
+    Html += '<div class="card-action login_header_colour">'
+    
+    Html += '<div class="row ">'
+    Html += '<div class="col s12 ">'
+    Html += '<div class="row"></div>'
+    Html += '</div>'
+    Html += '</div>'
+    
+    Html += '</div>'
+    Html += '</div>'
+    Html += '</div>'
+    Html += '</div>'
+    Html += '</div>'
+    
+    //Login card
+    Html += '<div class="row">'
+    Html += '<div class="col s12">'
+    Html += '<div class="card horizontal z-depth-4">'
+    Html += '<div class="card-stacked">'
+    Html += '<div class="card-action">'
+    
+    Html += '<div class="row">'
+    Html += '<div class="input-field col s12">'
+    Html += '<input id="" type="text" placeholder="Email">'
+    Html += '</div>'
+    Html += '</div>'
+    
+    Html += '<div class="row">'
+    Html += '<div class="input-field col s12">'
+    Html += '<input id="" type="text" placeholder="username">'
+    Html += '</div>'
+    Html += '</div>'
+    
+    Html += '<div class="row">'
+    Html += '<div class="input-field col s12">'
+    Html += '<input id="" type="text" placeholder="Password">'
+    Html += '</div>'
+    Html += '</div>'
+
+    Html += '<div class="row">'
+    Html += '<div class="input-field col s12">'
+    Html += '<input id="" type="text" placeholder="phone number">'
+    Html += '</div>'
+    Html += '</div>'
+
+    Html += '<div class="row">'
+    Html += '<div class=" col s12 waves-effect waves-light login_button login_button_button_colour">'
+    Html += '<div class="row">Register</div>'
+    Html += '</div>'
+    Html += '</div>'
+
+    Html += '</div>'
+    Html += '</div>'
+    Html += '</div>'
+    Html += '</div>'
+    Html += '</div>'
+
+    
+    //Register card
+    Html += '<div class="row">'
+    Html += '<div class="col s12">'
+    Html += '<div class="card horizontal z-depth-4">'
+    Html += '<div class="card-stacked">'
+    Html += '<div class="card-action">'
+    
+    Html += '<div class="row">'
+    Html += '<div class=" col s12">'
+    Html += '<label>Already a memeber</label>'
+    Html += '</div>'
+    Html += '</div>'
+    
+    Html += '<div class="row " onclick="displayLoginPage();">'
+    Html += '<div class=" col s12 waves-effect waves-light login_button memeber_now_button_colour ">'
+    Html += '<div class="row">login Now</div>'
+    Html += '</div>'
+    Html += '</div>'
+    
+    Html += '</div>'
+    Html += '</div>'
+    Html += '</div>'
+    Html += '</div>'
+    Html += '</div>'
+
+    window.setTimeout(function() {
+                      $('#register').html(Html)
+                      }, 200);
+    
+    
+    
+    
+    
+}
+
+
+
+
+
+
 
 
 
