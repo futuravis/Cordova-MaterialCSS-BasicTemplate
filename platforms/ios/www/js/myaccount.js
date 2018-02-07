9049f5d64c0c159fb22af022f617a93730cb9b95
ahtsmsr <mishra.ashutosh880@gmail.com>
2018-02-07 21:27:40 +0530

    commit my account with API
    commit my account with API

--- platforms/ios/www/js/myaccount.js
+++ platforms/ios/www/js/myaccount.js
@@ -4,12 +4,51 @@ function displayMyaccount(){
     
     slide_page('front','myaccount')
     
+    callProfileApi()
+    
+   
+    
+    
+    
+    
+    
+}
+
+function callProfileApi(){
+ 
+    var userId = localStorage.getItem("userid");
+
+    
+    $.ajax({
+           type : "GET",
+           crossDomain: true,
+           url : "https://tlgrmapp.com/apps/dev/classified/index.php/api/users/info/id/"+userId+"",
+           beforeSend: function(xhr){xhr.setRequestHeader('X-API-KEY', '1741B791EB86DF69C5A4338444AF5');},
+           //data: { "username" : email, "password" : password},
+           dataType: "json",
+           success : function(result) {
+           //$('.profile_name').html(result.username)
+           displayMyaccounset(result)
+           //set your variable to the result
+           //displayContent(result)
+           },
+           error : function(result) {
+           console.log("call Failed");
+           //handle the error
+           }
+           });
+}
+
+function displayMyaccounset(result){
+    
+   // alert(JSON.stringify(result))
+    
     var html= '';
     html+= backHeader('My Account','home');
     
     html += '<div class="profile_div">'
     html += '<div class="profile_img"><img src="img/man.png" width="140" style=" margin:3px 3px 2px 3px"/></div>'
-    html += '<div class="profile_name">Ashu</div>'
+    html += '<div class="profile_name">'+result.info.username+'</div>'
     html +='</div>'
     
     html += '<div class="row" style="margin-top:15px;">'
@@ -18,19 +57,24 @@ function displayMyaccount(){
     html += '<div class="card-stacked">'
     html += '<div class="card-action">'
     html += '<div style=" font-size: 16px; color: #70EE35">Email Address</div>'
-    html += '<div>john@gmail.com</div>'
+    html += '<div>'+result.info.email+'</div>'
     html += '</div>'
     html += '</div>'
     html += '</div>'
     html += '</div>'
     
+    var fname = "Not Set";
+    
+    if(result.info.name != null){
+    var fname = result.info.name + ' ' + result.info.surname
+    }
     html += '<div class="row">'
     html += '<div class="col s12">'
     html += '<div class="card horizontal z-depth-4">'
     html += '<div class="card-stacked">'
     html += '<div class="card-action">'
     html += '<div style=" font-size: 16px; color: #70EE35">Full Name</div>'
-    html += '<div>Ashutosh Mishra</div>'
+    html += '<div>'+fname+'</div>'
     html += '</div>'
     html += '</div>'
     html += '</div>'
@@ -42,7 +86,7 @@ function displayMyaccount(){
     html += '<div class="card-stacked">'
     html += '<div class="card-action">'
     html += '<div style=" font-size: 16px; color: #70EE35">Contact Number</div>'
-    html += '<div>5566445567</div>'
+    html += '<div>'+result.info.contact+'</div>'
     html += '</div>'
     html += '</div>'
     html += '</div>'
@@ -51,16 +95,8 @@ function displayMyaccount(){
     window.setTimeout(function() {
                       $('#myaccount').html(html)
                       }, 200);
-    
-    
-    
-    
-    
+
 }
-
-
-
-
 
 
 
